efcdec791ef3363f2732c9eed5628303684f4972
Federico <federicobono99@gmail.com>
2024-02-27 17:23:46 +0100

    Update

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ load_dotenv()
 
 project = Project.from_bump(
     bump_folder=os.getenv("BUMP_PATH"),
-    project_id="28be199c825d419957bc753a9519e8e9ecc6a08e"
+    project_id="9461431622cf39efe60cf1eb03a94083780c5720"
 )
 
 subprocess.run([
