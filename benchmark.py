33d1b5fb06410e3e06afe0abac01f5dc5469d7a8
Federico <federicobono99@gmail.com>
2024-03-13 15:58:26 +0100

    Change patches storage structure

--- benchmark.py
+++ benchmark.py
@@ -113,7 +113,7 @@ def run_benchmark(key: str, name: str, projects: List[Project], pipeline: Pipeli
 
 def move_patches(project: Project, path: str):
     os.makedirs(os.path.dirname(f"{path}/patches/"), exist_ok=True)
-    subprocess.call(f"mv {project.path}/patches/* {path}/patches", shell=True)
+    subprocess.call(f"mv {project.path}/patches/* {path}/{project.project_id}/patches", shell=True)
 
 def run_project(project: Project, pipeline: PipelineRunningMode, model: LLMType) -> ProjectRepairStatus:
     print(f"\n\n###### RUNNING PROJECT {project.project_name} ######")
