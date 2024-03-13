6f75e76b838baa9aefa92ca9daa326aa57c649c8
Federico <federicobono99@gmail.com>
2024-03-13 15:55:02 +0100

    [Benchmark] update results

--- test.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-
-try:
-    result = subprocess.run("sleep 10 && echo ciao", timeout=1, shell=True, stdout=subprocess.PIPE)
-    print(f"let's go: {result}")
-except subprocess.TimeoutExpired:
-    # insert code here
-    print("Timeout")
