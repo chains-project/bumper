6515448b996b10097d39e87ebe9520d253b85da3
Federico <federicobono99@gmail.com>
2024-04-04 15:30:47 +0200

    Update script

--- scripts/migrations/0001_add_initial_errors_count_field.bash
+++ scripts/migrations/0001_add_initial_errors_count_field.bash
@@ -4,4 +4,4 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" || exit ; pwd -P )
 cd "$parent_path" || exit
 cd ../..
 
-jq 'to_entries | map_values(.value += { "initial_errors_count": 0 }) | from_entries' $1 > $1
+echo $(cat $1 | jq 'to_entries | map_values(.value += { "initial_errors_count": 0 }) | from_entries' | jq .) > $1
